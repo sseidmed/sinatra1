74a6d65e86822e426345aef33152442398e931e1
Ubuntu <ubuntu@ip-172-31-84-63.ec2.internal>
2020-02-15 20:27:36 +0000

    modified sinatra file

--- sinatra1.rb
+++ sinatra1.rb
@@ -15,28 +15,27 @@ get '/' do
     "Welcome to Sinatra"
 end
 
-  post "/submitted" do
-    "<h1>Form Submitted</h1>"
-  end
+post "/submitted" do
+  "<h1>Form Submitted</h1>"
+end
 
 
-  post "/logon" do
+post "/logon" do
     if params[:username] == "bob" and params[:pwd] == "pass"
       "Success"
     else
       "Failure"
     end
-  end
+end
   
-  post "/hello" do
-     session[:session_message] = params[:name]
-     "Saved the name"
-     #"<h1>I love to eat #{session[:session_message]}</h1>"
-  end
+post "/hello" do
+   session[:session_message] = params[:name]
+   "Hello, #{session[:session_message]}! Nice to meet you!"
+end
   
-  get "/greet" do
-      "Hello, #{session[:session_message]}"
-  end
+get "/greet" do
+    "Hello, #{session[:session_message]}! Nice to meet you!"
+end
       
 
 
