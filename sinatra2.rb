c8d2241c880aa42308aeedf6fe939443fdae883b
Ubuntu <ubuntu@ip-172-31-84-63.ec2.internal>
2020-03-17 20:50:44 +0000

    sql and erb

--- sinatra2.rb
+++ sinatra2.rb
@@ -1,13 +1,27 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require "sqlite3"
+require "sinatra"
+require "sinatra/reloader"
 
-get '/' do
-    erb :howmany
+if File.exists?("maindata.db")
+    File.delete("maindata.db")
 end
 
-post '/candles' do
-    @number = params[:number].to_i
-    erb :candles
-    
-end
+db = SQLite3::Database.new("maindata.db")
 
+db.execute <<-SQL
+    CREATE TABLE users (
+        id integer PRIMARY KEY,
+        email VARCHAR,
+        password VARCHAR);
+SQL
+
+db.execute("INSERT INTO users (email, password) values (?, ?)", ['bobsample@gmail.com', 'pass123'])
+db.execute("INSERT INTO users (email, password) values (?, ?)", ['skylite@gmail.com', 'pass123'])
+db.execute("INSERT INTO users (email, password) values (?, ?)", ['williams_k@gmail.com', 'pass123'])
+db.execute("INSERT INTO users (email, password) values (?, ?)", ['computer_lab@gmail.com', 'pass123'])
+
+get '/' do 
+    @sql_array = db.execute("SELECT * FROM users")
+    erb :results
+end
+#puts sql_array
